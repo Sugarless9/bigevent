9abcfbe4fc79147c1f0f7ae8e588b0cfa8ead47a
Sugarless9 <374961428@qq.com>
2021-01-24 16:21:10 +0800

    登录注册完成

---
+++ assets/js/login.js
@@ -0,0 +1,50 @@
+$(function () {
+    $(`.loginBox a`).click(function () {
+        $(`.loginBox`).hide()
+        $(`.regiBox`).show()
+    });
+    $(`.regiBox a`).click(function () {
+        $(`.regiBox`).hide()
+        $(`.loginBox`).show()
+    });
+    let form = layui.form;
+    form.verify({
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+        pass: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        // 要求确认密码与密码一致
+        repass: function (value, item) { //value：表单的值、item：表单的DOM对象
+            let pwd = $(`#password`).val()
+            if (pwd !== value) {
+                return "两次密码不一致"
+            }
+        }
+    });
+    $(`.regiBox form`).on(`submit`, function (e) {
+        e.preventDefault()
+        let data = $(this).serialize()
+        axios.post(`/api/reguser`, data).then(function (res) {
+            if (res.data.status !== 0) {
+                return layer.msg(res.data.message)
+            }
+            console.log(`----`);
+            layer.msg(res.data.message)
+            $(`.regiBox a`).click()
+        })
+    })
+    $(`.loginBox form`).on(`submit`, function (e) {
+        e.preventDefault()
+        let data = $(this).serialize()
+        axios.post(`/api/login`, data).then(function (res) {
+            if (res.data.status !== 0) {
+                return layer.msg(res.data.message)
+            }
+            localStorage.setItem(`token`, res.data.token)
+            layer.msg(res.data.message, function () {
+                location.href = `/home/index.html`
+            })
+        })
+    })
+});
